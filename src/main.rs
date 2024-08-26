6105a0c6e5d64fd4076ebe44798ab432ae04efe5
kalyons11 <20310471+kalyons11@users.noreply.github.com>
2024-08-25 21:44:58 -0400

    Add rust files

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::{env, fs};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read to the file");
+
+    dbg!(contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        
+        Config {
+            query,
+            file_path
+        }
+    }
+}
